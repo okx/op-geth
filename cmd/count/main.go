49c923c49935d3f430638045f90432a0ce164667
xingqiang.yuan <xingqiang.yuan@okg.com>
2025-09-09 13:00:01 +0800

    debug

--- cmd/count/main.go
+++ cmd/count/main.go
@@ -98,7 +98,7 @@ func main() {
 	}
 	defer iter.Close()
 
-	if iter.SeekGE(configPrefix) {
+	if iter.SeekGE([]byte("ethereum")) {
 		key := iter.Key()
 		fmt.Printf("Found key: %s\n", hex.EncodeToString(key[:]))
 		value, err := iter.ValueAndErr()
