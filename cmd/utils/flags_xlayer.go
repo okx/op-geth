d26f77a78e081febdde9e7b35c845bb0f87dec08
lucas.lim <lucas.lim@okg.com>
2025-09-12 10:51:40 +0800

    fix: add okPay flags into main.go

--- cmd/utils/flags_xlayer.go
+++ cmd/utils/flags_xlayer.go
@@ -8,17 +8,17 @@ import (
 
 var (
 	// OkPay
-	OkPayPriorityEnableFlag = cli.BoolFlag{
+	OkPayPriorityEnableFlag = &cli.BoolFlag{
 		Name:  "okpay.priority-enable-flag",
 		Usage: "OkPay",
 		Value: false,
 	}
-	OkPaySenderAccountsList = cli.StringFlag{
+	OkPaySenderAccountsList = &cli.StringFlag{
 		Name:  "okpay.sender-accounts-list",
 		Usage: "List of OkPay sender accounts",
 		Value: "",
 	}
-	OkPayBlockPriorityTxsLimit = cli.Uint64Flag{
+	OkPayBlockPriorityTxsLimit = &cli.Uint64Flag{
 		Name:  "okpay.block-priority-txs-limit",
 		Usage: "Max number of OkPay txs that we will prioritize per block",
 		Value: 0,
@@ -26,9 +26,9 @@ var (
 
 	// XLayerFlags are the default flags for X Layer features
 	XLayerFlags = []cli.Flag{
-		&OkPayPriorityEnableFlag,
-		&OkPaySenderAccountsList,
-		&OkPayBlockPriorityTxsLimit,
+		OkPayPriorityEnableFlag,
+		OkPaySenderAccountsList,
+		OkPayBlockPriorityTxsLimit,
 	}
 )
 
