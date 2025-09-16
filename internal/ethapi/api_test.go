8bb151a53b1f04f5cc963abce90c1dcad0997b8d
ruiwei.guo <ruiwei.guo@okg.com>
2025-09-16 15:18:29 +0800

    fix

--- internal/ethapi/api_test.go
+++ internal/ethapi/api_test.go
@@ -1491,7 +1491,7 @@ func TestSimulateV1(t *testing.T) {
 		Data        hexutil.Bytes  `json:"data"`
 		BlockNumber hexutil.Uint64 `json:"blockNumber"`
 		// Skip txHash
-		//Hash common.Hash `json:"transactionHash" gencodec:"required"`
+		//TxHash common.Hash `json:"transactionHash" gencodec:"required"`
 		TxIndex hexutil.Uint `json:"transactionIndex"`
 		//BlockHash common.Hash  `json:"blockHash"`
 		Index hexutil.Uint `json:"logIndex"`
