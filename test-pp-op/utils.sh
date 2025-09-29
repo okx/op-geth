c117ca2aa046a0fff8f39faf323f7ce764f4f7ed
cliff.yang <cliff.yang@okg.com>
2025-09-29 14:36:04 +0800

    refactor

--- test-pp-op/utils.sh
+++ test-pp-op/utils.sh
@@ -12,14 +12,26 @@ setup_xlayer_erigon() {
   rm -rf $PWD_DIR/tmp/xlayer-erigon
   mkdir -p $PWD_DIR/tmp
   cd $PWD_DIR/tmp/
-  git clone -b feat/cliff/op-migration-test https://github.com/okx/xlayer-erigon.git
+   if [ ! -d "$PWD_DIR/tmp/xlayer-erigon" ]; then
+    echo "Cloning xlayer-erigon repository..."
+    mkdir -p $PWD_DIR/tmp
+    cd $PWD_DIR/tmp/
+    git clone -b feat/cliff/op-migration-test https://github.com/okx/xlayer-erigon.git
+  else
+    echo "xlayer-erigon directory already exists, skipping clone"
+  fi
 
   cd $PWD_DIR/tmp/xlayer-erigon/test-pp-op
+
+  if ! [ -f .env ]; then
+    cp example.env .env
+  fi
 
   if [ "$ENV" = "mainnet" ]; then
     make mainnet
   else
+    echo "Making local"
     make local
   fi
-  cd cd $PWD_DIR
+  cd $PWD_DIR
 }
