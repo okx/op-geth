a0c3c5a0ea980982f904f44b1819fddd093c9faf
Niven <sieniven@gmail.com>
2025-09-29 09:38:43 +0800

    Update

--- realtime/cache/plain_state_cache.go
+++ realtime/cache/plain_state_cache.go
@@ -47,6 +47,7 @@ func (cache *plainStateCache) Flatten(incoming *plainStateCache) {
 
 	for address := range incoming.deletedAccountsCache {
 		cache.deletedAccountsCache[address] = struct{}{}
+		delete(cache.accountCache, address)
 	}
 
 	// Apply code changes
