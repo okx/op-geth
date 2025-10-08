15084d290ab365ca6630b3e8f90868e668e8945f
JimmyShi22 <417711026@qq.com>
2025-10-08 10:42:52 +0800

    migrate gas price ut from erigon

---
+++ eth/gasprice/xlayer/follower_xlayer_test.go
@@ -0,0 +1,64 @@
+package xlayer
+
+import (
+	"context"
+	"math/big"
+	"testing"
+	"time"
+
+	"github.com/ethereum/go-ethereum/eth/gasprice"
+	"github.com/stretchr/testify/require"
+)
+
+func TestFollowerUpdateGasPrice(t *testing.T) {
+	ctx := context.Background()
+	var d time.Duration = 1000000000
+
+	cfg := gasprice.Config{
+		MaxPrice: new(big.Int).SetUint64(0),
+		XLayer: gasprice.XLayerGasPriceConfig{
+			Type:               gasprice.GasPriceFollowerType,
+			UpdatePeriod:       d,
+			KafkaURL:           "127.0.0.1:9092",
+			Topic:              "middle_coinPrice_push",
+			Factor:             0.5,
+			DefaultL1CoinPrice: 1,
+			DefaultL2CoinPrice: 1,
+			Default:            new(big.Int).SetUint64(1000000000),
+		},
+	}
+	l1GasPrice := big.NewInt(10000000000)
+	f := newFollowerGasPriceSuggester(ctx, cfg)
+
+	f.UpdateGasPriceAvg(l1GasPrice)
+
+	// Calculate correct GP
+	// Correct GP: L1/L2 ratio = 1, l1GasPrice * 1 * Factor
+	correctGpVal := big.NewFloat(0).Mul(big.NewFloat(0).SetFloat64(cfg.XLayer.Factor), big.NewFloat(0).SetInt(l1GasPrice))
+	correctGp := new(big.Int)
+	correctGpVal.Int(correctGp)
+	require.Equal(t, correctGp.Uint64(), f.GetLastRawGP().Uint64())
+}
+
+func TestMaxGasPriceLimit(t *testing.T) {
+	ctx := context.Background()
+	var d time.Duration = 1000000000
+
+	cfg := gasprice.Config{
+		MaxPrice: new(big.Int).SetUint64(50000000),
+		XLayer: gasprice.XLayerGasPriceConfig{
+			Type:               gasprice.GasPriceFollowerType,
+			UpdatePeriod:       d,
+			KafkaURL:           "127.0.0.1:9092",
+			Topic:              "middle_coinPrice_push",
+			Factor:             0.5,
+			DefaultL1CoinPrice: 1,
+			DefaultL2CoinPrice: 1,
+			Default:            new(big.Int).SetUint64(100000000),
+		},
+	}
+	l1GasPrice := big.NewInt(1000000000)
+	f := newFollowerGasPriceSuggester(ctx, cfg)
+	f.UpdateGasPriceAvg(l1GasPrice)
+	require.Equal(t, f.GetLastRawGP().Uint64(), cfg.MaxPrice.Uint64())
+}
