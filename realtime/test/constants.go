53a1ee7d5b5079b0e2fd815bc3ebe0170e059405
Niven <sieniven@gmail.com>
2025-10-08 15:55:32 +0800

    Fix stress test

--- realtime/test/constants.go
+++ realtime/test/constants.go
@@ -3,10 +3,11 @@ package test
 import "time"
 
 const (
-	DefaultL2NetworkRealtimeURL          = "http://localhost:8124"
-	DefaultL2NetworkNoRealtimeURL        = "http://localhost:8128"
-	DefaultL2NetworkWSURL                = "ws://localhost:7547"
-	DefaultL2ChainID              uint64 = 196
+	DefaultL2NetworkRealtimeSeqURL        = "http://localhost:8123"
+	DefaultL2NetworkRealtimeURL           = "http://localhost:8124"
+	DefaultL2NetworkNoRealtimeURL         = "http://localhost:8128"
+	DefaultL2NetworkWSURL                 = "ws://localhost:7547"
+	DefaultL2ChainID               uint64 = 196
 
 	DefaultL2AdminAddress    = "0x8f8E2d6cF621f30e9a11309D6A56A876281Fd534"
 	DefaultL2AdminPrivateKey = "0x815405dddb0e2a99b12af775fd2929e526704e1d1aea6a0b4e74dc33e2f7fcd2"
