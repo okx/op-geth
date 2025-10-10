4571eea36b6cb387f53431c7aa03bf1b22d7ce02
Vui-Chee <vuicheesiew@gmail.com>
2025-10-10 17:01:59 +0800

    retab

--- test-pp-op/5-all.sh
+++ test-pp-op/5-all.sh
@@ -8,12 +8,12 @@ start_local_container() {
   else
 		CONTAINER_ID=$(docker run \
       --name $CONTAINER_NAME \
-  		-v /var/run/docker.sock:/var/run/docker.sock \
+			-v /var/run/docker.sock:/var/run/docker.sock \
 			-v "$(pwd):/app/op-geth/test-pp-op" \
-  		-v "$(pwd)/data/op-geth-seq:/app/op-geth/test-pp-op/data/op-geth-seq" \
-  		-v "$(pwd)/data/cannon-data:/app/op-program/bin" \
-  		-e DOCKER_HOST=unix:///var/run/docker.sock \
-  		-d op-migrate:latest sleep infinity)
+			-v "$(pwd)/data/op-geth-seq:/app/op-geth/test-pp-op/data/op-geth-seq" \
+			-v "$(pwd)/data/cannon-data:/app/op-program/bin" \
+			-e DOCKER_HOST=unix:///var/run/docker.sock \
+			-d op-migrate:latest sleep infinity)
   fi
 }
 
@@ -92,9 +92,9 @@ done
 prompt_before_continue() {
 	read -p "Do you want to continue? (y/n): " answer
 	case $answer in
-  	[Yy]* ) echo "Continuing...";;
-  	[Nn]* ) echo "Exiting..."; exit;;
-  	* ) echo "Invalid input";;
+		[Yy]* ) echo "Continuing...";;
+		[Nn]* ) echo "Exiting..."; exit;;
+		* ) echo "Invalid input";;
 	esac
 }
 
@@ -105,28 +105,28 @@ if [ "$ENV" = "local" ];then
 	start_local_container
 
 	if (( STAGE <= 1 ));then
-  	docker exec $CONTAINER_ID bash -c "
-  		cd /app/op-geth/test-pp-op
-  		./5-1-migrate-prepare.sh
+		docker exec $CONTAINER_ID bash -c "
+			cd /app/op-geth/test-pp-op
+			./5-1-migrate-prepare.sh
 		"
 		prompt_before_continue
 	fi
 
 	if (( STAGE <= 2 ));then
-  	docker exec $CONTAINER_ID bash -c "
-  		cd /app/op-geth/test-pp-op
-  		./5-2-migrate-op.sh
+		docker exec $CONTAINER_ID bash -c "
+			cd /app/op-geth/test-pp-op
+			./5-2-migrate-op.sh
 			echo 'Compressing merged.genesis.json...'
-  		gzip -c merged.genesis.json > config-op/merged.genesis.gz.json
-  		cp config-op/rollup.json /app/op-program/chainconfig/configs/195-rollup.json
-  		cp config-op/merged.genesis.gz.json /app/op-program/chainconfig/configs/195-genesis-l2.json
+			gzip -c merged.genesis.json > config-op/merged.genesis.gz.json
+			cp config-op/rollup.json /app/op-program/chainconfig/configs/195-rollup.json
+			cp config-op/merged.genesis.gz.json /app/op-program/chainconfig/configs/195-genesis-l2.json
 		"
 		prompt_before_continue
 	fi
 
 	if (( STAGE <= 3 ));then
-  	docker exec $CONTAINER_ID bash -c "
-  		cd /app
+		docker exec $CONTAINER_ID bash -c "
+			cd /app
 			make reproducible-prestate
 		"
 	fi
@@ -137,17 +137,17 @@ if [ "$ENV" = "testnet" ];then
 	start_testnet_container
 
 	if (( STAGE <= 1 ));then
-  	docker exec $CONTAINER_ID bash -c "
-  		cd /app/op-geth/test-pp-op
-  		./5-1-migrate-prepare.sh
+		docker exec $CONTAINER_ID bash -c "
+			cd /app/op-geth/test-pp-op
+			./5-1-migrate-prepare.sh
 		"
 		prompt_before_continue
 	fi
 
 	if (( STAGE <= 2 ));then
-  	docker exec $CONTAINER_ID bash -c "
-  		cd /app/op-geth/test-pp-op
-  		./5-2-migrate-op.sh
+		docker exec $CONTAINER_ID bash -c "
+			cd /app/op-geth/test-pp-op
+			./5-2-migrate-op.sh
 			gzip -c merged.genesis.json > config-op/merged.genesis.gz.json                              
 			echo 'Compressing merged.genesis.json...'
 			cp config-op/rollup.json /app/op-program/chainconfig/configs/196-rollup.json                
@@ -158,10 +158,10 @@ if [ "$ENV" = "testnet" ];then
 	fi
 
 	if (( STAGE <= 3 ));then
-  	docker exec $CONTAINER_ID bash -c "
-  		cd /app
+		docker exec $CONTAINER_ID bash -c "
+			cd /app
 			make reproducible-prestate
-	    cp -rfv /app/op-geth/test-pp-op/* /app/op-geth/test-pp-op/.* /mnt/ramdisk_op/test-pp-op
+			cp -rfv /app/op-geth/test-pp-op/* /app/op-geth/test-pp-op/.* /mnt/ramdisk_op/test-pp-op
 		"
 	fi
 fi
