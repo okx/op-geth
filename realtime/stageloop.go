3ab4d3e3ad0f7b3e24fb1553c068d84521dcd813
Niven <sieniven@gmail.com>
2025-10-18 15:58:03 +0800

    Fix logging

--- realtime/stageloop.go
+++ realtime/stageloop.go
@@ -54,7 +54,7 @@ func ListenRealtimeProducer(
 					log.Error(fmt.Sprintf("[Realtime] Failed to send error trigger message. error: %v, currHeight: %d", err, currHeight))
 				}
 			} else {
-				log.Debug(fmt.Sprintf("[Realtime] Sent kafka confirmed block info message for block number %d, blockHash: %x", currHeight, blockInfo.Hash))
+				log.Debug(fmt.Sprintf("[Realtime] Sent kafka block info message for block number %d, blockHash: %x", currHeight, blockInfo.Hash))
 			}
 		case txInfo := <-txInfoChan:
 			currHeight = txInfo.BlockNumber
