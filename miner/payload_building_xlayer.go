34877b68278b95bf1e170c93fbb6876c40e8a3fd
Niven <sieniven@gmail.com>
2025-10-22 11:49:44 +0800

    Fix

--- miner/payload_building_xlayer.go
+++ miner/payload_building_xlayer.go
@@ -5,7 +5,5 @@ import "github.com/ethereum/go-ethereum/beacon/engine"
 func (payload *Payload) resolveRealtime() *engine.ExecutionPayloadEnvelope {
 	// Realtime incremental building requires block building to be completed.
 	// We block until the block is fully built before returning.
-	payload.lock.Lock()
-	defer payload.lock.Unlock()
 	return payload.resolve(true)
 }
