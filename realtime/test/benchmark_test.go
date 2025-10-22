67b60d2b40e0e99e3d45735d038f0e89f475880f
Niven <sieniven@gmail.com>
2025-10-22 15:31:58 +0800

    Add logging tx index

--- realtime/test/benchmark_test.go
+++ realtime/test/benchmark_test.go
@@ -53,6 +53,7 @@ func TestRealtimeBenchmarkNativeTransferConfirmation(t *testing.T) {
 
 		// Send tx
 		signedTx := nativeTransferTx(t, context.Background(), client, big.NewInt(Gwei), testAddress.String())
+		fmt.Printf("Sent tx: %s\n", signedTx.Hash().String())
 
 		// Run state benchmark
 		g, ctx := errgroup.WithContext(ctx)
@@ -134,6 +135,7 @@ func TestRealtimeBenchmarkERC20TransferConfirmation(t *testing.T) {
 		require.Equal(t, ethBalance.String(), realtimeBalance.String())
 
 		signedTx := erc20TransferTx(t, ctx, privateKey, client, transferAmount, nil, testAddress, erc20Address, startNonce+uint64(i))
+		fmt.Printf("Sent tx: %s\n", signedTx.Hash().String())
 
 		// Run state benchmark
 		g, ctx := errgroup.WithContext(ctx)
