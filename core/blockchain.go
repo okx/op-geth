08f018ec8be223d6b3e4c56fb914957bb209e5e3
fine <litesky@foxmail.com>
2025-10-28 07:53:49 +0000

    fix log

--- core/blockchain.go
+++ core/blockchain.go
@@ -2211,12 +2211,6 @@ func (bc *BlockChain) processBlock(parentRoot common.Hash, block *types.Block, s
 		monitor.StepBlockchainFinalize.Key, blockHeight, blockHash, block.Time(),
 		0, "finalized", "", res.GasUsed)
 
-	// Try merge propose stats snapshot if exists (and add propose time into final block time)
-	if pstat, ok := metrics.GlobalStatsStore.GetAndDelete(block.Hash()); ok {
-		_ = ls.CombinedSummary(pstat)
-	} else {
-		ls.CombinedSummary(nil)
-	}
 	logStatistic(block, statedb, startTime, ptime, vtime, triehash, trieUpdate, xvtime, wstart, proctime)
 
 	return &blockProcessingResult{
