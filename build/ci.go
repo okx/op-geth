d0105274787d8ddc48f07e8a59813b834c2e5032
brendontan03 <brendontan003@gmail.com>
2025-11-07 16:25:25 +0800

    remove changes made for linting flags

--- build/ci.go
+++ build/ci.go
@@ -442,7 +442,7 @@ func doLint(cmdline []string) {
 	}
 
 	linter := downloadLinter(*cachedir)
-	lflags := []string{"run", "--config", ".golangci.yml", "--enable-all", "--timeout", "10m", "--verbose"}
+	lflags := []string{"run", "--config", ".golangci.yml"}
 	build.MustRunCommandWithOutput(linter, append(lflags, packages...)...)
 	fmt.Println("You have achieved perfection.")
 }
