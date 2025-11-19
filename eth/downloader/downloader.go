2c61242963aa5fe17a40334ea91bc03d504f4e87
KyrinCode <kyrincode@qq.com>
2025-11-20 01:02:58 +0800

    support el-sync by integrating legacyXLayerBlock

--- eth/downloader/downloader.go
+++ eth/downloader/downloader.go
@@ -243,7 +243,12 @@ func New(stateDb ethdb.Database, mux *event.TypeMux, chain BlockChain, dropPeer 
 		syncStartBlock:    chain.CurrentSnapBlock().Number.Uint64(),
 	}
 	// Create the post-merge skeleton syncer and start the process
-	dl.skeleton = newSkeleton(stateDb, dl.peers, dropPeer, newBeaconBackfiller(dl, success))
+	// For XLayer, get legacyXLayerBlock from chainConfig
+	var legacyXLayerBlock uint64
+	if chain.Config().LegacyXLayerBlock != nil {
+		legacyXLayerBlock = chain.Config().LegacyXLayerBlock.Uint64()
+	}
+	dl.skeleton = newSkeleton(stateDb, dl.peers, dropPeer, newBeaconBackfiller(dl, success), legacyXLayerBlock)
 
 	go dl.stateFetcher()
 	return dl
