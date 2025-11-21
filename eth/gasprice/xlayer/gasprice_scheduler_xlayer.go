c5cd41e0078da4f84a64f0f27d2a9d9bf8e9185f
lucas.lim <lucas.lim@okg.com>
2025-11-21 10:58:49 +0800

    fix: remove wrong file commit

--- eth/gasprice/xlayer/gasprice_scheduler_xlayer.go
+++ eth/gasprice/xlayer/gasprice_scheduler_xlayer.go
@@ -9,7 +9,6 @@ import (
 
 	"github.com/ethereum/go-ethereum/core"
 	"github.com/ethereum/go-ethereum/log"
-	"github.com/ethereum/go-ethereum/xlayer/apollo"
 )
 
 // GasPriceOracle defines the interface for gas price suggestions
@@ -108,9 +107,7 @@ func (s *XLayerScheduler) runL2GasPriceSuggester() {
 		log.Debug("L1 gas price has not been set, please start op-node", "err", err)
 	}
 
-	log.Info(fmt.Sprintf("[Debug] gasprice_scheduler_xlayer.go line 111"))
-
-	updateTimer := time.NewTimer(5 * time.Second)
+	updateTimer := time.NewTimer(s.gpricer.GetConfig().XLayer.UpdatePeriod)
 	defer updateTimer.Stop()
 
 	for {
@@ -122,89 +119,6 @@ func (s *XLayerScheduler) runL2GasPriceSuggester() {
 			log.Info("Stopping l2 gas price suggester...")
 			return
 		case <-updateTimer.C:
-			log.Info("[Debug] gasprice_scheduler_xlayer.go line 122, updateTimer.C")
-
-			test, ok := apollo.ApolloConfigOr(apollo.L2GasPricer, "gpo.maxprice", s.gpricer.GetConfig().MaxPrice.String())
-			if !ok {
-				log.Error("[Debug] line 130 failed to get maxprice from apollo")
-			}
-			log.Info(fmt.Sprintf("[Debug] gasprice_scheduler_xlayer.go line 59, test: %d", test))
-
-			// Test 1: int64
-			testI64, ok := apollo.ApolloConfigOr(apollo.L2GasPricer, "test.int64", int64(999))
-			if !ok {
-				log.Error("[Debug] line 138 failed to get int64 from apollo")
-			}
-			log.Info("[Lucas] [Debug] int64 - after", "result", testI64)
-
-			// Test 2: uint64
-			testU64, ok := apollo.ApolloConfigOr(apollo.L2GasPricer, "test.uint64", uint64(888))
-			if !ok {
-				log.Error("[Debug] line 142 failed to get uint64 from apollo")
-			}
-			log.Info("[Lucas] [Debug] uint64 - after", "result", testU64)
-
-			// Test 3: bool
-			testBool, ok := apollo.ApolloConfigOr(apollo.L2GasPricer, "test.bool", true)
-			if !ok {
-				log.Error("[Debug] line 146 failed to get bool from apollo")
-			}
-			log.Info("[Lucas] [Debug] bool - after", "result", testBool)
-
-			// Test 4: string
-			testStr, ok := apollo.ApolloConfigOr(apollo.L2GasPricer, "test.string", "default-value")
-			if !ok {
-				log.Error("[Debug] line 150 failed to get string from apollo")
-			}
-			log.Info("[Lucas] [Debug] string - after", "result", testStr)
-
-			// Test 5: float64
-			testF64, ok := apollo.ApolloConfigOr(apollo.L2GasPricer, "test.float64", 3.14159)
-			if !ok {
-				log.Error("[Debug] line 154 failed to get float64 from apollo")
-			}
-			log.Info("[Lucas] [Debug] float64 - after", "result", testF64)
-
-			// Test 6: *big.Int (small)
-			defaultSmall := big.NewInt(100000001)
-			testBigSmall, ok := apollo.ApolloConfigOr(apollo.L2GasPricer, "test.bigint.small", defaultSmall.String())
-			if !ok {
-				log.Error("[Debug] line 158 failed to get *big.Int (small) from apollo")
-			}
-			newBigIntSmall, _ := new(big.Int).SetString(testBigSmall, 10)
-			log.Info("[Lucas] [Debug] *big.Int (small) - after", "result", newBigIntSmall)
-
-			// Test 7: *big.Int (large)
-			defaultLarge := new(big.Int)
-			defaultLarge.SetString("5000000000000000000003", 10)
-			testBigLarge, ok := apollo.ApolloConfigOr(apollo.L2GasPricer, "test.bigint.large", defaultLarge.String())
-			if !ok {
-				log.Error("[Debug] line 162 failed to get *big.Int (large) from apollo")
-			}
-			newBigIntLarge, _ := new(big.Int).SetString(testBigLarge, 10)
-			log.Info("[Lucas] [Debug] *big.Int (large) - after", "result", newBigIntLarge)
-
-			// Test 8: Missing key (should return default)
-			testMissing, ok := apollo.ApolloConfigOr(apollo.L2GasPricer, "test.nonexistent", int64(12345))
-			if !ok {
-				log.Error("[Debug] line 166 failed to get missing key from apollo")
-			}
-			log.Info("[Lucas] [Debug] missing key - after", "result", testMissing, "should_be_default", testMissing == 12345)
-
-			// Test 9: array
-			testArray, ok := apollo.ApolloConfigOr(apollo.L2GasPricer, "test.array", []int{1, 2, 3})
-			if !ok {
-				log.Error("[Debug] line 170 failed to get array from apollo")
-			}
-			log.Info("[Lucas] [Debug] array - after", "result", testArray)
-
-			// Test 10: array of strings
-			testArrayOfStrings, ok := apollo.ApolloConfigOr(apollo.L2GasPricer, "test.array.of.strings", []string{"a", "b", "c"})
-			if !ok {
-				log.Error("[Debug] line 174 failed to get array of strings from apollo")
-			}
-			log.Info("[Lucas] [Debug] array of strings - after", "result", testArrayOfStrings)
-
 			// Check if eth and blockchain are available
 			if s.eth == nil || s.eth.Blockchain() == nil {
 				log.Error("blockchain is not available")
