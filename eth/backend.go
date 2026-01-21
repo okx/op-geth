f723b6796c5668e971d3d0ac86d6b14b839613e5
JimmyShi22 <417711026@qq.com>
2026-01-21 17:25:45 +0800

    rm unnecessary comments

--- eth/backend.go
+++ eth/backend.go
@@ -145,7 +145,6 @@ type Ethereum struct {
 }
 
 // New creates a new Ethereum object (including the initialisation of the common Ethereum object),
-// whose lifecycle will be managed by the provided node.
 func New(stack *node.Node, config *ethconfig.Config) (*Ethereum, error) {
 	// Ensure configuration values are compatible and sane
 	if !config.SyncMode.IsValid() {
