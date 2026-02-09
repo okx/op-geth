dddc3994607fc66fb8cef9ebd6ac0ab880ea82b6
brendontan03 <brendontan003@gmail.com>
2026-02-09 17:30:29 +0800

    fix linting errors

--- core/vm/interpreter.go
+++ core/vm/interpreter.go
@@ -42,7 +42,7 @@ type Config struct {
 
 	PrecompileOverrides PrecompileOverrides                   // Precompiles can be swapped / changed / wrapped as needed
 	NoMaxCodeSize       bool                                  // Ignore Max code size and max init code size limits
-	CallerOverride func(v common.Address) common.Address // Swap the caller as needed, for VM prank functionality.
+	CallerOverride      func(v common.Address) common.Address // Swap the caller as needed, for VM prank functionality.
 }
 
 // ScopeContext contains the things that are per-call, such as stack and memory,
