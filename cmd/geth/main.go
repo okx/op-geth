f5fbf58f649e36bd6a201915b1ceb344ee5963e0
weixie.cui <weixie.cui@okg.com>
2026-06-17 21:56:47 +0800

    feat: gasless tx eviction

--- cmd/geth/main.go
+++ cmd/geth/main.go
@@ -174,6 +174,7 @@ var (
 		utils.RollupSuperchainUpgradesFlag,
 		utils.RollupAllowGaslessFlag,
 		utils.RollupGaslessMockGasPricePercentileFlag,
+		utils.RollupGaslessLifetimeFlag,
 		configFileFlag,
 		utils.LogDebugFlag,
 		utils.LogBacktraceAtFlag,
